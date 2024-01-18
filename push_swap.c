755ee69af33468259e2a82d946cd15c3aa020c70
Annika Makela <amakela@c2r1p5.hive.fi>
2024-01-18 23:00:20 +0200

    push_swap

---
+++ push_swap.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 17:13:34 by amakela           #+#    #+#             */
+/*   Updated: 2024/01/18 20:12:39 by amakela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "push_swap.h"
+
+void	push_swap(stack_node *a)
+{
+	stack_node *b;
+
+	b = NULL;
+	if (is_sorted(a))
+		return ;
+}
